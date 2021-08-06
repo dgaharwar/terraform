1887cb34c6b93b74ac232029bd83479faebd54d8
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2021-08-06 16:55:11 +0100

    Update variable.tf

--- terraform_illapa/variable.tf
+++ terraform_illapa/variable.tf
@@ -1,11 +1,11 @@
 variable "vnet_name" {
-description = "Name of the vnet to create"
-default = "acctvnet"
+  description = "Name of the vnet to create"
+  default = "acctvnet"
 }
 
 variable "resource_group_name" {
-description = "Default resource group name that the network will be created in."
-default = "morph-demo"
+  description = "Default resource group name that the network will be created in."
+  default = "morph-demo"
 }
 
 #variable "location" {
@@ -13,44 +13,47 @@ default = "morph-demo"
 #}
 
 variable "address_space" {
-description = "The address space that is used by the virtual network."
-default = "10.1.0.0/16"
+  description = "The address space that is used by the virtual network."
+  default = "10.1.0.0/16"
 }
 
 # If no values specified, this defaults to Azure DNS
 variable "dns_servers" {
-description = "The DNS servers to be used with vNet."
-default = []
+  description = "The DNS servers to be used with vNet."
+  default = []
 }
 
 variable "subnet_prefixes" {
-description = "The address prefix to use for the subnet."
-default = ["10.0.0.0/24"]
+  description = "The address prefix to use for the subnet."
+  default = ["10.0.0.0/24"]
 }
 
 variable "subnet_names" {
-description = "A list of public subnets inside the vNet."
-default = ["morphdemoVnet - morphDemoSubnet"]
+  description = "A list of public subnets inside the vNet."
+  default = ["morphdemoVnet - morphDemoSubnet"]
 }
 
 variable "tags" {
-description = "The tags to associate with your network and subnets."
-type = map(string)
-default = [tag1 = "",tag2 = ""]
+  description = "The tags to associate with your network and subnets."
+  type = map(string)
+  default = {
+    tag1 = "Illapa"
+    tag2 = "Cloud"
+  }
 }
 
 variable "allow_rdp_traffic" {
-description = "This optional variable, when set to true, adds a security rule allowing RDP traffic to flow through to the newly created network. The default $"
-default = false
+  description = "This optional variable, when set to true, adds a security rule allowing RDP traffic to flow through to the newly created network. The default $"
+  default = false
 }
 
 variable "allow_ssh_traffic" {
-description = "This optional variable, when set to true, adds a security rule allowing SSH traffic to flow through to the newly created network. The default $"
-default = false
+  description = "This optional variable, when set to true, adds a security rule allowing SSH traffic to flow through to the newly created network. The default $"
+  default = false
 }
 
 
 variable "sg_name" {
-description = "Give a name to security group"
-default = "morph-demo-sg"
+  description = "Give a name to security group"
+  default = "morph-demo-sg"
 }
