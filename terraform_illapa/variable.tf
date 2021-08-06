d8f0be93295c856f10b0a28411eb9f1e1c17fa57
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2021-08-06 14:51:28 +0100

    Update variable.tf

--- terraform_illapa/variable.tf
+++ terraform_illapa/variable.tf
@@ -1,36 +1,37 @@
 variable "vnet_name" {
 description = "Name of the vnet to create"
-default = "dg-vNet"
+default = []
 }
 
 variable "resource_group_name" {
 description = "Default resource group name that the network will be created in."
-default = "dg-ops"
+default = []
 }
 
 variable "location" {
 description = "The location/region where the core network will be created. The full list of Azure regions can be found at https://azure.microsoft.com/regions"
+defalut = []
 }
 
 variable "address_space" {
 description = "The address space that is used by the virtual network."
-default = "10.0.0.0/16"
+default = []
 }
 
 # If no values specified, this defaults to Azure DNS
 variable "dns_servers" {
 description = "The DNS servers to be used with vNet."
-default = "dgdns"
+default = []
 }
 
 variable "subnet_prefixes" {
 description = "The address prefix to use for the subnet."
-default = "10.0.0.0/24"
+default = []
 }
 
 variable "subnet_names" {
 description = "A list of public subnets inside the vNet."
-default = "dgmorphsubnet"
+default = []
 }
 
 #variable "tags" {
@@ -55,5 +56,5 @@ default = "false"
 
 variable "sg_name" {
 description = "Give a name to security group"
-default = "dg-nsg"
+default = []
 }
