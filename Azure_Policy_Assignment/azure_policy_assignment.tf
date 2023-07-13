b2000e453cbb60d2a7435d7c1748b4236a824d44
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2023-07-13 20:21:59 +0100

    Update azure_policy_assignment.tf

--- Azure_Policy_Assignment/azure_policy_assignment.tf
+++ Azure_Policy_Assignment/azure_policy_assignment.tf
@@ -9,20 +9,20 @@ features {}
     version = "=2.5.0"
 }
 
-# Configire the data
-data "azurerm_resource_group" "example" {
+# Configure the data
+data "azurerm_resource_group" "dg_rg" {
     name     = "rmg-ops"
 }
 
-# Configire the resources
-resource "azurerm_policy_assignment" "example" {
+# Configure the resources
+resource "azurerm_policy_assignment" "dg_policy" {
     name                 = "View and configure system diagnostic data"
-    scope                = data.azurerm_resource_group.example.id
+    scope                = data.azurerm_resource_group.dg_rg.id
     policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/0015ea4d-51ff-4ce3-8d8c-f3f8f0179a56"
 }
 
 
-# Configire the Variables
+# Configure the Variables
 variable subscription_id {
     type = string
     default = ""
