e77b3c0e2606cd08dd3fe785cf2087810ca4af66
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2023-07-13 20:19:57 +0100

    Update azure_policy_assignment.tf

--- Azure_Policy_Assignment/azure_policy_assignment.tf
+++ Azure_Policy_Assignment/azure_policy_assignment.tf
@@ -1,7 +1,6 @@
 # Configure the Microsoft Azure Provider
 provider "azurerm" { 
 features {}
-#   use ENV VARS
     subscription_id = var.subscription_id
     tenant_id       = var.tenant_id
     client_id       = var.client_id
