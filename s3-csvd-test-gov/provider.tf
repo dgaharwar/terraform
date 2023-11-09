8b06938adfd4c25b3674186aa227d23d0ffd67cb
Deepti gaharwar <deepti.gaharwar@gmail.com>
2023-11-09 20:37:06 +0000

    update

--- s3-csvd-test-gov/provider.tf
+++ s3-csvd-test-gov/provider.tf
@@ -3,9 +3,9 @@ variable "aws_secret_key" {}
 variable "aws_access_key" {}
 
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.aws_region
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
 }
 
 terraform {
