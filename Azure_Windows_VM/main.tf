631e6b92f0e798807f561e317f09847bcd624e35
Deepti gaharwar <deepti.gaharwar@gmail.com>
2023-11-17 22:39:21 +0000

    update

--- Azure_Windows_VM/main.tf
+++ Azure_Windows_VM/main.tf
@@ -169,6 +169,7 @@ resource "azurerm_windows_virtual_machine" "vm" {
   size                = var.vm_size
   admin_username      = var.admin_username
   admin_password      = var.admin_password
+  custom_data         = base64encode(local.custom_data)
   network_interface_ids = [
     azurerm_network_interface.nic.id,
   ]
