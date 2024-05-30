ccf279f7e475245d91ea204510ed4decdace8ca8
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2024-05-30 15:58:27 +0100

    Add files via upload

---
+++ EC2_with_multiple_EBS_volumes/main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_instance" "dgexample" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  tags = {
+    Name = "dgexample-instance"
+  }
+
+  dynamic "ebs_block_device" {
+    for_each = { for key, drive in var.additional_drive_configuration : drive.name => drive }
+
+    content {
+      device_name           = ebs_block_device.value.name
+      volume_size           = ebs_block_device.value.size
+      volume_type           = var.volume_type
+      delete_on_termination = var.delete_on_termination
+      kms_key_id            = aws_kms_key.ebs_kms_key.arn
+      encrypted             = true
+    }
+  }
+
+  root_block_device {
+    volume_size = var.root_volume_size
+    volume_type = var.volume_type
+  }
+}
+
+resource "aws_kms_key" "ebs_kms_key" {
+  description             = "KMS key for EBS volume encryption"
+  deletion_window_in_days = 10
+}
---
+++ EC2_with_multiple_EBS_volumes/variables.tf
@@ -0,0 +1,54 @@
+variable "region" {
+  description = "The AWS region to create resources in"
+  default     = "us-east-1"
+}
+
+variable "access_key" {
+  description = "The Access Key for the user to authenticate"
+}
+
+variable "region" {
+  description = "The Access Key for the user to authenticate"
+  default     = "us-east-1"
+}
+
+variable "ami" {
+  description = "The AMI ID to use for the instance"
+  default     = "ami-0e001c9271cf7f3b9"
+}
+
+variable "instance_type" {
+  description = "The type of instance to create"
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "The key pair name to use for the instance"
+  default     = "MorpheusApp"
+}
+
+variable "volume_type" {
+  description = "The type of EBS volume"
+  default     = "gp2"
+}
+
+variable "delete_on_termination" {
+  description = "Whether the volume should be deleted on instance termination"
+  default     = true
+}
+
+variable "root_volume_size" {
+  description = "The size of the root volume"
+  default     = 8
+}
+
+variable "additional_drive_configuration" {
+  description = "Configuration for additional EBS volumes"
+  type        = list(object({
+    name         = string
+    size         = number
+    drive_letter = string
+    drive_name   = string
+  }))
+  default = []
+}
