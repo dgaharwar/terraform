f0f819a991d411652ccdac4d7f02da639ab2356c
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2024-06-02 15:20:36 +0100

    Update providers.tf

--- ngdc-morpheus-blueprint-EC2-Win/providers.tf
+++ ngdc-morpheus-blueprint-EC2-Win/providers.tf
@@ -13,9 +13,11 @@ terraform {
 
 provider "aws" {
   region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
 
-  assume_role {
-    role_arn     = "arn:aws-us-gov:iam::${var.account_id}:role/NGDCMorpheusAdminRole"
-    session_name = "Morpheus_Terraform"
-  }
+#  assume_role {
+#    role_arn     = "arn:aws-us-gov:iam::${var.account_id}:role/NGDCMorpheusAdminRole"
+#    session_name = "Morpheus_Terraform"
+#  }
 }
