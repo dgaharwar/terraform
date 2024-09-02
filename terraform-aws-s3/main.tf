20efa7c926e83958c599ceb291fed02f1b78275a
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2024-09-02 16:17:48 +0100

    Update main.tf

--- terraform-aws-s3/main.tf
+++ terraform-aws-s3/main.tf
@@ -702,30 +702,30 @@ data "aws_iam_policy_document" "require_latest_tls" {
   }
 }
 
-resource "aws_s3_bucket_intelligent_tiering_configuration" "this" {
-  for_each = { for k, v in local.intelligent_tiering : k => v if local.create_bucket }
+#resource "aws_s3_bucket_intelligent_tiering_configuration" "this" {
+#  for_each = { for k, v in local.intelligent_tiering : k => v if local.create_bucket }
 
-  name   = each.key
-  bucket = aws_s3_bucket.this[0].id
-  status = try(tobool(each.value.status) ? "Enabled" : "Disabled", title(lower(each.value.status)), null)
+#  name   = each.key
+#  bucket = aws_s3_bucket.this[0].id
+#  status = try(tobool(each.value.status) ? "Enabled" : "Disabled", title(lower(each.value.status)), null)
 
-  # Max 1 block - filter
-  dynamic "filter" {
-    for_each = length(try(flatten([each.value.filter]), [])) == 0 ? [] : [true]
+#  # Max 1 block - filter
+#  dynamic "filter" {
+#    for_each = length(try(flatten([each.value.filter]), [])) == 0 ? [] : [true]
 
-    content {
-      prefix = try(each.value.filter.prefix, null)
-      tags   = try(each.value.filter.tags, null)
-    }
-  }
+#    content {
+#      prefix = try(each.value.filter.prefix, null)
+#      tags   = try(each.value.filter.tags, null)
+#    }
+#  }
 
-  dynamic "tiering" {
-    for_each = each.value.tiering
+#  dynamic "tiering" {
+#    for_each = each.value.tiering
 
-    content {
-      access_tier = tiering.key
-      days        = tiering.value.days
-    }
-  }
+#    content {
+#      access_tier = tiering.key
+#      days        = tiering.value.days
+#    }
+#  }
 
-}
+#}
