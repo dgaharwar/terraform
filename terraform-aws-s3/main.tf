b957ad5c32f6aa27e6f2b2f3f97d32d7d794f968
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2024-09-02 15:55:36 +0100

    Update main.tf

--- terraform-aws-s3/main.tf
+++ terraform-aws-s3/main.tf
@@ -14,10 +14,10 @@ locals {
   attach_policy = var.attach_require_latest_tls_policy || var.attach_elb_log_delivery_policy || var.attach_lb_log_delivery_policy || var.attach_deny_insecure_transport_policy || var.attach_policy
 
   # Variables with type `any` should be jsonencode()'d when value is coming from Terragrunt
-  grants              = try(jsondecode(var.grant), var.grant)
-  cors_rules          = try(jsondecode(var.cors_rule), var.cors_rule)
-  lifecycle_rules     = try(jsondecode(var.lifecycle_rule), var.lifecycle_rule)
-  intelligent_tiering = try(jsondecode(var.intelligent_tiering), var.intelligent_tiering)
+#  grants              = try(jsondecode(var.grant), var.grant)
+#  cors_rules          = try(jsondecode(var.cors_rule), var.cors_rule)
+#  lifecycle_rules     = try(jsondecode(var.lifecycle_rule), var.lifecycle_rule)
+#  intelligent_tiering = try(jsondecode(var.intelligent_tiering), var.intelligent_tiering)
 }
 
 resource "aws_s3_bucket" "this" {
