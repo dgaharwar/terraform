8c12ce5ffd01ccdea71403ad854221bde21f4049
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2024-09-02 16:33:58 +0100

    Update main.tf

--- terraform-aws-s3/main.tf
+++ terraform-aws-s3/main.tf
@@ -383,187 +383,187 @@ resource "aws_s3_bucket_object_lock_configuration" "this" {
   }
 }
 
-resource "aws_s3_bucket_replication_configuration" "this" {
-  count = local.create_bucket && length(keys(var.replication_configuration)) > 0 ? 1 : 0
+#resource "aws_s3_bucket_replication_configuration" "this" {
+#  count = local.create_bucket && length(keys(var.replication_configuration)) > 0 ? 1 : 0
 
-  bucket = aws_s3_bucket.this[0].id
-  role   = var.replication_configuration["role"]
+#  bucket = aws_s3_bucket.this[0].id
+#  role   = var.replication_configuration["role"]
 
-  dynamic "rule" {
-    for_each = flatten(try([var.replication_configuration["rule"]], [var.replication_configuration["rules"]], []))
+#  dynamic "rule" {
+#    for_each = flatten(try([var.replication_configuration["rule"]], [var.replication_configuration["rules"]], []))
 
-    content {
-      id       = try(rule.value.id, null)
-      priority = try(rule.value.priority, null)
-      prefix   = try(rule.value.prefix, null)
-      status   = try(tobool(rule.value.status) ? "Enabled" : "Disabled", title(lower(rule.value.status)), "Enabled")
+#    content {
+#      id       = try(rule.value.id, null)
+#      priority = try(rule.value.priority, null)
+#      prefix   = try(rule.value.prefix, null)
+#      status   = try(tobool(rule.value.status) ? "Enabled" : "Disabled", title(lower(rule.value.status)), "Enabled")
 
-      dynamic "delete_marker_replication" {
-        for_each = flatten(try([rule.value.delete_marker_replication_status], [rule.value.delete_marker_replication], []))
+#      dynamic "delete_marker_replication" {
+#        for_each = flatten(try([rule.value.delete_marker_replication_status], [rule.value.delete_marker_replication], []))
 
-        content {
+#        content {
           # Valid values: "Enabled" or "Disabled"
-          status = try(tobool(delete_marker_replication.value) ? "Enabled" : "Disabled", title(lower(delete_marker_replication.value)))
-        }
-      }
+#          status = try(tobool(delete_marker_replication.value) ? "Enabled" : "Disabled", title(lower(delete_marker_replication.value)))
+#        }
+#      }
 
       # Amazon S3 does not support this argument according to:
       # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_replication_configuration
       # More infor about what does Amazon S3 replicate?
       # https://docs.aws.amazon.com/AmazonS3/latest/userguide/replication-what-is-isnot-replicated.html
-      dynamic "existing_object_replication" {
-        for_each = flatten(try([rule.value.existing_object_replication_status], [rule.value.existing_object_replication], []))
+#      dynamic "existing_object_replication" {
+#        for_each = flatten(try([rule.value.existing_object_replication_status], [rule.value.existing_object_replication], []))
 
-        content {
+#        content {
           # Valid values: "Enabled" or "Disabled"
-          status = try(tobool(existing_object_replication.value) ? "Enabled" : "Disabled", title(lower(existing_object_replication.value)))
-        }
-      }
+#          status = try(tobool(existing_object_replication.value) ? "Enabled" : "Disabled", title(lower(existing_object_replication.value)))
+#        }
+#      }
 
-      dynamic "destination" {
-        for_each = try(flatten([rule.value.destination]), [])
+#      dynamic "destination" {
+#        for_each = try(flatten([rule.value.destination]), [])
 
-        content {
-          bucket        = destination.value.bucket
-          storage_class = try(destination.value.storage_class, null)
-          account       = try(destination.value.account_id, destination.value.account, null)
+#        content {
+#          bucket        = destination.value.bucket
+#          storage_class = try(destination.value.storage_class, null)
+#          account       = try(destination.value.account_id, destination.value.account, null)
 
-          dynamic "access_control_translation" {
-            for_each = try(flatten([destination.value.access_control_translation]), [])
+#          dynamic "access_control_translation" {
+#            for_each = try(flatten([destination.value.access_control_translation]), [])
 
-            content {
-              owner = title(lower(access_control_translation.value.owner))
-            }
-          }
+#            content {
+#              owner = title(lower(access_control_translation.value.owner))
+#           }
+#         }
 
-          dynamic "encryption_configuration" {
-            for_each = flatten([try(destination.value.encryption_configuration.replica_kms_key_id, destination.value.replica_kms_key_id, [])])
+#          dynamic "encryption_configuration" {
+#            for_each = flatten([try(destination.value.encryption_configuration.replica_kms_key_id, destination.value.replica_kms_key_id, [])])
 
-            content {
-              replica_kms_key_id = encryption_configuration.value
-            }
-          }
+#            content {
+#              replica_kms_key_id = encryption_configuration.value
+#            }
+#          }
 
-          dynamic "replication_time" {
-            for_each = try(flatten([destination.value.replication_time]), [])
+#          dynamic "replication_time" {
+#            for_each = try(flatten([destination.value.replication_time]), [])
 
-            content {
+#            content {
               # Valid values: "Enabled" or "Disabled"
-              status = try(tobool(replication_time.value.status) ? "Enabled" : "Disabled", title(lower(replication_time.value.status)), "Disabled")
+#              status = try(tobool(replication_time.value.status) ? "Enabled" : "Disabled", title(lower(replication_time.value.status)), "Disabled")
 
-              dynamic "time" {
-                for_each = try(flatten([replication_time.value.minutes]), [])
+#              dynamic "time" {
+#                for_each = try(flatten([replication_time.value.minutes]), [])
 
-                content {
-                  minutes = replication_time.value.minutes
-                }
-              }
-            }
+#                content {
+#                 minutes = replication_time.value.minutes
+#                }
+#             }
+#           }
 
-          }
+#          }
 
-          dynamic "metrics" {
-            for_each = try(flatten([destination.value.metrics]), [])
+#          dynamic "metrics" {
+#            for_each = try(flatten([destination.value.metrics]), [])
 
-            content {
+#            content {
+#              # Valid values: "Enabled" or "Disabled"
+#              status = try(tobool(metrics.value.status) ? "Enabled" : "Disabled", title(lower(metrics.value.status)), "Disabled")
+
+#              dynamic "event_threshold" {
+#                for_each = try(flatten([metrics.value.minutes]), [])
+
+#                content {
+#                  minutes = metrics.value.minutes
+#                }
+#              }
+#            }
+#          }
+#        }
+#      }
+
+#      dynamic "source_selection_criteria" {
+#        for_each = try(flatten([rule.value.source_selection_criteria]), [])
+
+#        content {
+#          dynamic "replica_modifications" {
+#            for_each = flatten([try(source_selection_criteria.value.replica_modifications.enabled, source_selection_criteria.value.replica_modifications.status, [])])
+
+#            content {
               # Valid values: "Enabled" or "Disabled"
-              status = try(tobool(metrics.value.status) ? "Enabled" : "Disabled", title(lower(metrics.value.status)), "Disabled")
+#              status = try(tobool(replica_modifications.value) ? "Enabled" : "Disabled", title(lower(replica_modifications.value)), "Disabled")
+#            }
+#          }
 
-              dynamic "event_threshold" {
-                for_each = try(flatten([metrics.value.minutes]), [])
+#          dynamic "sse_kms_encrypted_objects" {
+#            for_each = flatten([try(source_selection_criteria.value.sse_kms_encrypted_objects.enabled, source_selection_criteria.value.sse_kms_encrypted_objects.status, [])])
 
-                content {
-                  minutes = metrics.value.minutes
-                }
-              }
-            }
-          }
-        }
-      }
-
-      dynamic "source_selection_criteria" {
-        for_each = try(flatten([rule.value.source_selection_criteria]), [])
-
-        content {
-          dynamic "replica_modifications" {
-            for_each = flatten([try(source_selection_criteria.value.replica_modifications.enabled, source_selection_criteria.value.replica_modifications.status, [])])
-
-            content {
+#            content {
               # Valid values: "Enabled" or "Disabled"
-              status = try(tobool(replica_modifications.value) ? "Enabled" : "Disabled", title(lower(replica_modifications.value)), "Disabled")
-            }
-          }
-
-          dynamic "sse_kms_encrypted_objects" {
-            for_each = flatten([try(source_selection_criteria.value.sse_kms_encrypted_objects.enabled, source_selection_criteria.value.sse_kms_encrypted_objects.status, [])])
-
-            content {
-              # Valid values: "Enabled" or "Disabled"
-              status = try(tobool(sse_kms_encrypted_objects.value) ? "Enabled" : "Disabled", title(lower(sse_kms_encrypted_objects.value)), "Disabled")
-            }
-          }
-        }
-      }
+#              status = try(tobool(sse_kms_encrypted_objects.value) ? "Enabled" : "Disabled", title(lower(sse_kms_encrypted_objects.value)), "Disabled")
+#            }
+#          }
+#        }
+#      }
 
       # Max 1 block - filter - without any key arguments or tags
-      dynamic "filter" {
-        for_each = length(try(flatten([rule.value.filter]), [])) == 0 ? [true] : []
+#      dynamic "filter" {
+#        for_each = length(try(flatten([rule.value.filter]), [])) == 0 ? [true] : []
 
-        content {
-        }
-      }
+#        content {
+#        }
+#      }
 
       # Max 1 block - filter - with one key argument or a single tag
-      dynamic "filter" {
-        for_each = [for v in try(flatten([rule.value.filter]), []) : v if max(length(keys(v)), length(try(rule.value.filter.tags, rule.value.filter.tag, []))) == 1]
+#      dynamic "filter" {
+#        for_each = [for v in try(flatten([rule.value.filter]), []) : v if max(length(keys(v)), length(try(rule.value.filter.tags, rule.value.filter.tag, []))) == 1]
 
-        content {
-          prefix = try(filter.value.prefix, null)
+#        content {
+#          prefix = try(filter.value.prefix, null)
 
-          dynamic "tag" {
-            for_each = try(filter.value.tags, filter.value.tag, [])
+#          dynamic "tag" {
+#            for_each = try(filter.value.tags, filter.value.tag, [])
 
-            content {
-              key   = tag.key
-              value = tag.value
-            }
-          }
-        }
-      }
+#            content {
+#              key   = tag.key
+#              value = tag.value
+#            }
+#          }
+#        }
+#      }
 
       # Max 1 block - filter - with more than one key arguments or multiple tags
-      dynamic "filter" {
-        for_each = [for v in try(flatten([rule.value.filter]), []) : v if max(length(keys(v)), length(try(rule.value.filter.tags, rule.value.filter.tag, []))) > 1]
+#      dynamic "filter" {
+#        for_each = [for v in try(flatten([rule.value.filter]), []) : v if max(length(keys(v)), length(try(rule.value.filter.tags, rule.value.filter.tag, []))) > 1]
 
-        content {
-          and {
-            prefix = try(filter.value.prefix, null)
-            tags   = try(filter.value.tags, filter.value.tag, null)
-          }
-        }
-      }
-    }
-  }
+#        content {
+#          and {
+#            prefix = try(filter.value.prefix, null)
+#            tags   = try(filter.value.tags, filter.value.tag, null)
+#          }
+#        }
+#      }
+#    }
+#  }
 
   # Must have bucket versioning enabled first
-  depends_on = [aws_s3_bucket_versioning.this]
-}
+#  depends_on = [aws_s3_bucket_versioning.this]
+#}
 
-data "aws_iam_policy_document" "combined" {
-  count = local.create_bucket && local.attach_policy ? 1 : 0
+#data "aws_iam_policy_document" "combined" {
+#  count = local.create_bucket && local.attach_policy ? 1 : 0
 
-  source_policy_documents = compact([
-    var.attach_elb_log_delivery_policy ? data.aws_iam_policy_document.elb_log_delivery[0].json : "",
-    var.attach_lb_log_delivery_policy ? data.aws_iam_policy_document.lb_log_delivery[0].json : "",
-    var.attach_require_latest_tls_policy ? data.aws_iam_policy_document.require_latest_tls[0].json : "",
-    var.attach_deny_insecure_transport_policy ? data.aws_iam_policy_document.deny_insecure_transport[0].json : "",
-    var.attach_policy ? var.policy : ""
-  ])
-}
+#  source_policy_documents = compact([
+#    var.attach_elb_log_delivery_policy ? data.aws_iam_policy_document.elb_log_delivery[0].json : "",
+#    var.attach_lb_log_delivery_policy ? data.aws_iam_policy_document.lb_log_delivery[0].json : "",
+#    var.attach_require_latest_tls_policy ? data.aws_iam_policy_document.require_latest_tls[0].json : "",
+#    var.attach_deny_insecure_transport_policy ? data.aws_iam_policy_document.deny_insecure_transport[0].json : "",
+#    var.attach_policy ? var.policy : ""
+#  ])
+#}
 
 # AWS Load Balancer access log delivery policy
-#data "aws_elb_service_account" "this" {
-#  count = local.create_bucket && var.attach_elb_log_delivery_policy ? 1 : 0
+data "aws_elb_service_account" "this" {
+  count = local.create_bucket && var.attach_elb_log_delivery_policy ? 1 : 0
 #}
 
 #data "aws_iam_policy_document" "elb_log_delivery" {
