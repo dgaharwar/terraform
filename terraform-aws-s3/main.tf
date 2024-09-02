4ee31d35086c28e97e69654b51f5d1075611824a
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2024-09-02 16:13:54 +0100

    Update main.tf

--- terraform-aws-s3/main.tf
+++ terraform-aws-s3/main.tf
@@ -172,21 +172,21 @@ resource "aws_s3_bucket_website_configuration" "this" {
   }
 }
 
-resource "aws_s3_bucket_versioning" "this" {
-  count = local.create_bucket && length(keys(var.versioning)) > 0 ? 1 : 0
+#resource "aws_s3_bucket_versioning" "this" {
+#  count = local.create_bucket && length(keys(var.versioning)) > 0 ? 1 : 0
 
-  bucket                = aws_s3_bucket.this[0].id
-  expected_bucket_owner = var.expected_bucket_owner
-  mfa                   = try(var.versioning["mfa"], null)
+#  bucket                = aws_s3_bucket.this[0].id
+#  expected_bucket_owner = var.expected_bucket_owner
+#  mfa                   = try(var.versioning["mfa"], null)
 
-  versioning_configuration {
-    # Valid values: "Enabled" or "Suspended"
-    status = try(var.versioning["enabled"] ? "Enabled" : "Suspended", tobool(var.versioning["status"]) ? "Enabled" : "Suspended", title(lower(var.versioning["status"])))
+#  versioning_configuration {
+#    # Valid values: "Enabled" or "Suspended"
+#    status = try(var.versioning["enabled"] ? "Enabled" : "Suspended", tobool(var.versioning["status"]) ? "Enabled" : "Suspended", title(lower(var.versioning["status"])))
 
-    # Valid values: "Enabled" or "Disabled"
-    mfa_delete = try(tobool(var.versioning["mfa_delete"]) ? "Enabled" : "Disabled", title(lower(var.versioning["mfa_delete"])), null)
-  }
-}
+#    # Valid values: "Enabled" or "Disabled"
+#    mfa_delete = try(tobool(var.versioning["mfa_delete"]) ? "Enabled" : "Disabled", title(lower(var.versioning["mfa_delete"])), null)
+#  }
+#}
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   count = local.create_bucket && length(keys(var.server_side_encryption_configuration)) > 0 ? 1 : 0
