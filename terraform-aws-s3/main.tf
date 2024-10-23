5e91db0ef63a524949cbf4af51291506eb316afc
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2024-10-23 17:43:42 +0100

    Update main.tf

--- terraform-aws-s3/main.tf
+++ terraform-aws-s3/main.tf
@@ -189,29 +189,29 @@ data "aws_partition" "current" {}
 #  }
 #}
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
-  count = local.create_bucket && length(keys(var.server_side_encryption_configuration)) > 0 ? 1 : 0
+#resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+#  count = local.create_bucket && length(keys(var.server_side_encryption_configuration)) > 0 ? 1 : 0
 
-  bucket                = aws_s3_bucket.this[0].id
-  expected_bucket_owner = var.expected_bucket_owner
+#  bucket                = aws_s3_bucket.this[0].id
+#  expected_bucket_owner = var.expected_bucket_owner
 
-  dynamic "rule" {
-    for_each = try(flatten([var.server_side_encryption_configuration["rule"]]), [])
+#  dynamic "rule" {
+#    for_each = try(flatten([var.server_side_encryption_configuration["rule"]]), [])
 
-    content {
-      bucket_key_enabled = try(rule.value.bucket_key_enabled, null)
+#    content {
+#      bucket_key_enabled = try(rule.value.bucket_key_enabled, null)
 
-      dynamic "apply_server_side_encryption_by_default" {
-        for_each = try([rule.value.apply_server_side_encryption_by_default], [])
+#      dynamic "apply_server_side_encryption_by_default" {
+#        for_each = try([rule.value.apply_server_side_encryption_by_default], [])
 
-        content {
-          sse_algorithm     = apply_server_side_encryption_by_default.value.sse_algorithm
-          kms_master_key_id = try(apply_server_side_encryption_by_default.value.kms_master_key_id, null)
-        }
-      }
-    }
-  }
-}
+#        content {
+#          sse_algorithm     = apply_server_side_encryption_by_default.value.sse_algorithm
+#          kms_master_key_id = try(apply_server_side_encryption_by_default.value.kms_master_key_id, null)
+#        }
+#      }
+#    }
+#  }
+#}
 
 resource "aws_s3_bucket_accelerate_configuration" "this" {
   count = local.create_bucket && var.acceleration_status != null ? 1 : 0
