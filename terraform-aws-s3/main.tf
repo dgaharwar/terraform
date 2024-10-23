2b323b7f76dddaf86644dbaf97f14562f4431646
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2024-10-23 17:39:20 +0100

    Update main.tf

--- terraform-aws-s3/main.tf
+++ terraform-aws-s3/main.tf
@@ -118,60 +118,60 @@ data "aws_partition" "current" {}
 #  depends_on = [aws_s3_bucket_ownership_controls.this]
 #}
 
-resource "aws_s3_bucket_website_configuration" "this" {
-  count = local.create_bucket && length(keys(var.website)) > 0 ? 1 : 0
+#resource "aws_s3_bucket_website_configuration" "this" {
+#  count = local.create_bucket && length(keys(var.website)) > 0 ? 1 : 0
 
-  bucket                = aws_s3_bucket.this[0].id
-  expected_bucket_owner = var.expected_bucket_owner
+#  bucket                = aws_s3_bucket.this[0].id
+#  expected_bucket_owner = var.expected_bucket_owner
 
-  dynamic "index_document" {
-    for_each = try([var.website["index_document"]], [])
+#  dynamic "index_document" {
+#    for_each = try([var.website["index_document"]], [])
 
-    content {
-      suffix = index_document.value
-    }
-  }
+#    content {
+#      suffix = index_document.value
+#    }
+#  }
 
-  dynamic "error_document" {
-    for_each = try([var.website["error_document"]], [])
+#  dynamic "error_document" {
+#    for_each = try([var.website["error_document"]], [])
 
-    content {
-      key = error_document.value
-    }
-  }
+#    content {
+#      key = error_document.value
+#    }
+#  }
 
-  dynamic "redirect_all_requests_to" {
-    for_each = try([var.website["redirect_all_requests_to"]], [])
+#  dynamic "redirect_all_requests_to" {
+#    for_each = try([var.website["redirect_all_requests_to"]], [])
 
-    content {
-      host_name = redirect_all_requests_to.value.host_name
-      protocol  = try(redirect_all_requests_to.value.protocol, null)
-    }
-  }
+#    content {
+#      host_name = redirect_all_requests_to.value.host_name
+#      protocol  = try(redirect_all_requests_to.value.protocol, null)
+#    }
+#  }
 
-  dynamic "routing_rule" {
-    for_each = try(flatten([var.website["routing_rules"]]), [])
+#  dynamic "routing_rule" {
+#    for_each = try(flatten([var.website["routing_rules"]]), [])
 
-    content {
-      dynamic "condition" {
-        for_each = [try([routing_rule.value.condition], [])]
+#    content {
+#      dynamic "condition" {
+#        for_each = [try([routing_rule.value.condition], [])]
 
-        content {
-          http_error_code_returned_equals = try(routing_rule.value.condition["http_error_code_returned_equals"], null)
-          key_prefix_equals               = try(routing_rule.value.condition["key_prefix_equals"], null)
-        }
-      }
+#        content {
+#          http_error_code_returned_equals = try(routing_rule.value.condition["http_error_code_returned_equals"], null)
+#          key_prefix_equals               = try(routing_rule.value.condition["key_prefix_equals"], null)
+#        }
+#      }
 
-      redirect {
-        host_name               = try(routing_rule.value.redirect["host_name"], null)
-        http_redirect_code      = try(routing_rule.value.redirect["http_redirect_code"], null)
-        protocol                = try(routing_rule.value.redirect["protocol"], null)
-        replace_key_prefix_with = try(routing_rule.value.redirect["replace_key_prefix_with"], null)
-        replace_key_with        = try(routing_rule.value.redirect["replace_key_with"], null)
-      }
-    }
-  }
-}
+#      redirect {
+#        host_name               = try(routing_rule.value.redirect["host_name"], null)
+#        http_redirect_code      = try(routing_rule.value.redirect["http_redirect_code"], null)
+#        protocol                = try(routing_rule.value.redirect["protocol"], null)
+#        replace_key_prefix_with = try(routing_rule.value.redirect["replace_key_prefix_with"], null)
+#        replace_key_with        = try(routing_rule.value.redirect["replace_key_with"], null)
+#      }
+#    }
+#  }
+#}
 
 resource "aws_s3_bucket_versioning" "this" {
   count = local.create_bucket && length(keys(var.versioning)) > 0 ? 1 : 0
