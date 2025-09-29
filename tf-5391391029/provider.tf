fec9a14dcc8088410e33803cb22affd1e0a3b527
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2025-09-29 21:56:49 +0100

    Update provider.tf

--- tf-5391391029/provider.tf
+++ tf-5391391029/provider.tf
@@ -1,16 +1,8 @@
 terraform {
   required_providers {
-#    http = {
-#      source  = "hashicorp/http"
-#      version = "~> 3.4.2"
-#    }
     nutanix = {
       source  = "nutanix/nutanix"
       version = "~> 1"
     }
-#    infoblox = {
-#      source  = "infobloxopen/infoblox"
-#      version = "~> 2"
-#    }
   }
 }
