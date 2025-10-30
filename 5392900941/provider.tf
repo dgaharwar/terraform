6d74844b3526498cc7281593a18d6b8d0b67e497
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2025-10-30 16:19:55 +0000

    Update provider.tf

--- 5392900941/provider.tf
+++ 5392900941/provider.tf
@@ -11,7 +11,7 @@ provider "nutanix" {
   username     = var.nutanix_username
   password     = var.nutanix_password
   endpoint     = var.nutanix_endpoint
-  port         = var.port
+  port         = 9440
   insecure     = true
   wait_timeout = 10
 }
